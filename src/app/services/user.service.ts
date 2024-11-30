cb884de8664ca4b7d7d9a6cb8ca9123f39c47543
alexandrov5433 <alexandrov5433@gmail.com>
2024-11-30 20:28:27 +0100

    Observable functionality prepared for later implementation of http requests.

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -17,16 +18,39 @@ export class UserService {
   }
 
   loginAs(status: string | undefined | null) {
-    if (!status) {
-      console.error(`UserService: Status is not of type string or is an empty string. Status: "${status}"`);
-      return;
-    }
-    this.isLoggedIn = true;
-    this.userAuthStat = status;
+    return new Observable((subscriber) => {
+      try {
+        if (!status) {
+          throw new Error(`UserService: Status is not of type string or is an empty string. Status: "${status}"`);
+        }
+        //TODO http req comes here
+        this.isLoggedIn = true;
+        this.userAuthStat = status;
+        subscriber.complete();
+        // setTimeout(() => {
+        //   subscriber.complete();
+        //   // subscriber.error(new Error('Checking slowly.'));
+        // }, 2000);
+        // throw new Error('asdadsasd');
+      } catch (e) {
+        subscriber.error(e);
+      }
+    });
   }
 
-  logout(): void {
-    this.isLoggedIn = false;
-    this.userAuthStat = null;
+  logout() {
+    return new Observable((subscriber) => {
+      try {
+        // if (!status) {
+        //   throw new Error(`UserService: Status is not of type string or is an empty string. Status: "${status}"`);
+        // }
+        //TODO http req comes here
+        this.isLoggedIn = false;
+        this.userAuthStat = null;
+        subscriber.complete();
+      } catch (e) {
+        subscriber.error(e);
+      }
+    });
   }
 }
