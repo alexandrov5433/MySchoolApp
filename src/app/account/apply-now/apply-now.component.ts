a52c5e24e1140d0f0befe699463c81e1ec69fdc7
alexandrov5433 <alexandrov5433@gmail.com>
2024-11-30 10:40:16 +0100

    ApplyNow component implemented.

---
+++ src/app/account/apply-now/apply-now.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apply-now',
+  imports: [],
+  templateUrl: './apply-now.component.html',
+  styleUrls: ['./apply-now.component.css', '../../shared/styles/customFileUploadComponent.css'],
+  standalone: true
+})
+export class ApplyNowComponent {
+
+}
