e07e190ddc6f08ce14d904da62599fe48c50e818
alexandrov5433 <alexandrov5433@gmail.com>
2024-11-30 10:40:53 +0100

    Routes for login, register and apply now created and working.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { TodoPageComponent } from './basic/todo-page/todo-page.component';
 import { LoginComponent } from './account/login/login.component';
+import { RegisterComponent } from './account/register/register.component';
+import { ApplyNowComponent } from './account/apply-now/apply-now.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
@@ -9,10 +11,10 @@ export const routes: Routes = [
     {path: 'contact', redirectTo: '/todo-page'},
     {path: 'faq', redirectTo: '/todo-page'},
     {path: 'forms-and-documents', redirectTo: '/todo-page'},
-    {path: 'log-in', component: LoginComponent},
-    {path: 'register', redirectTo: '/todo-page'},
-    {path: 'apply-now', redirectTo: '/todo-page'},
 
+    {path: 'log-in', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'apply-now', component: ApplyNowComponent},
 
     {path: 'todo-page', component: TodoPageComponent},
 ];
