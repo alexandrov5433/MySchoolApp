23a40c5bc36ff919dcc9d767319fe2ab7a31cf95
alexandrov5433 <alexandrov5433@gmail.com>
2024-11-30 20:53:25 +0100

    Loader and form controls implemented.

--- src/app/account/register/register.component.ts
+++ src/app/account/register/register.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { LoaderComponent } from '../../shared/loader/loader.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule, LoaderComponent, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
   standalone: true
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit, OnDestroy {
+  registerForm = new FormGroup({
+    registerAs: new FormControl(''),
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    dateOfBirth: new FormControl(''),
+    email: new FormControl(''),
+    mobileNumber: new FormControl(''),
+    homeNumber: new FormControl(''),
+    street: new FormControl(''),
+    houseNumber: new FormControl(''),
+    city: new FormControl(''),
+    authenticationCode: new FormControl(''),
+    profilePicture: new FormControl(''),
+  });
+  isLoading = false;
+  constructor() {}
 
+  register():void {
+    console.log(this.registerForm);
+    
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  ngOnDestroy(): void {
+    
+  }
 }
