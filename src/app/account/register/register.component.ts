f5a6d547d7c1897da2377cc86aed2351434ea986
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-01 15:02:10 +0100

    Field validations and file selection added.

--- src/app/account/register/register.component.ts
+++ src/app/account/register/register.component.ts
@@ -1,7 +1,20 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { LoaderComponent } from '../../shared/loader/loader.component';
 import { RouterLink } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { emailValidator } from '../../util/validators/email.validator';
+import { lastNameValidator } from '../../util/validators/last-name.validator';
+import { streetValidator } from '../../util/validators/street.validator';
+import { houseNumberValidator } from '../../util/validators/house-number.validator';
+import { mobileNumberValidator } from '../../util/validators/mobile-number.validator';
+import { cityValidator } from '../../util/validators/city.validator';
+import { validationLib } from '../../types/validationLib';
+import { homeNumberValidator } from '../../util/validators/home-number.validator';
+import { registerAsValidator } from '../../util/validators/register-as.validator';
+import { dateOfBirthValidator } from '../../util/validators/date-of-birth.validator';
+import { authenticationCodeValidator } from '../../util/validators/authentication-code.validator';
+import { firstNameValidator } from '../../util/validators/first-name.validator';
 
 @Component({
   selector: 'app-register',
@@ -12,32 +25,145 @@ import { RouterLink } from '@angular/router';
 })
 export class RegisterComponent implements OnInit, OnDestroy {
   registerForm = new FormGroup({
-    registerAs: new FormControl(''),
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    dateOfBirth: new FormControl(''),
-    email: new FormControl(''),
-    mobileNumber: new FormControl(''),
-    homeNumber: new FormControl(''),
-    street: new FormControl(''),
-    houseNumber: new FormControl(''),
-    city: new FormControl(''),
-    authenticationCode: new FormControl(''),
+    registerAs: new FormControl('', [registerAsValidator()]),
+    firstName: new FormControl('', [firstNameValidator()]),
+    lastName: new FormControl('', [lastNameValidator()]),
+    dateOfBirth: new FormControl('', [dateOfBirthValidator()]),
+    email: new FormControl('', [emailValidator()]),
+    mobileNumber: new FormControl('', [mobileNumberValidator()]),
+    homeNumber: new FormControl('', [homeNumberValidator()]),
+    street: new FormControl('', [streetValidator()]),
+    houseNumber: new FormControl('', [houseNumberValidator()]),
+    city: new FormControl('', [cityValidator()]),
+    authenticationCode: new FormControl('', [authenticationCodeValidator()]),
     profilePicture: new FormControl(''),
   });
   isLoading = false;
-  constructor() {}
+  profilePictureFileName: string | null = null;
+  profilePictureFile: File | null = null;
+  private ngUnsub = new Subject();
+  validationLib: validationLib = {
+    registerAs: {
+      validationClass: '',
+      showErrMsg: false
+    },
+    firstName: {
+      validationClass: '',
+      showErrMsg: false
+    },
+    lastName: {
+      validationClass: '',
+      showErrMsg: false
+    },
+    dateOfBirth: {
+      validationClass: '',
+      showErrMsg: false
+    },
+    email: {
+      validationClass: '',
+      showErrMsg: false
+    },
+    mobileNumber: {
+      validationClass: '',
+      showErrMsg: false
+    },
+    homeNumber: {
+      validationClass: '',
+      showErrMsg: false
+    },
+    street: {
+      validationClass: '',
+      showErrMsg: false
+    },
+    houseNumber: {
+      validationClass: '',
+      showErrMsg: false
+    },
+    city: {
+      validationClass: '',
+      showErrMsg: false
+    },
+    authenticationCode: {
+      validationClass: '',
+      showErrMsg: false
+    },
+    profilePicture: {
+      showErrMsg: false
+    }
+  };
+  constructor() { }
 
-  register():void {
+  register(): void {
     console.log(this.registerForm);
-    
+  }
+
+  isTouched(control: string): boolean | undefined {
+    return this.registerForm.get(control)?.touched;
+  }
+
+  isMissingValue(control: string): boolean | undefined {
+    return this.registerForm.get(control)?.value === '';
+  }
+
+  get isRegisterFormInvalid(): boolean {
+    if (this.registerForm.touched) {
+      return this.registerForm.invalid;
+    }
+    return true;
   }
 
   ngOnInit(): void {
-    
+    // form validation
+    ['registerAs',
+      'firstName',
+      'lastName',
+      'dateOfBirth',
+      'email',
+      'mobileNumber',
+      'homeNumber',
+      'street',
+      'houseNumber',
+      'city',
+      'authenticationCode'].forEach(control => {
+        this.registerForm.get(control)?.valueChanges
+          .pipe(takeUntil(this.ngUnsub))
+          .subscribe(x => {
+            const validatorName = control + 'Validator';
+            if (this.registerForm.get(control)?.errors?.[validatorName]) {
+              (this.validationLib as any)[control].validationClass = 'false-input';
+              (this.validationLib as any)[control].showErrMsg = true;
+            } else {
+              (this.validationLib as any)[control].validationClass = 'correct-input';
+              (this.validationLib as any)[control].showErrMsg = false;
+            }
+          });
+      });
+  }
+
+  // file input management for profile picture
+  onFileSelect(event: any) { 
+    if(!event.target.files[0]) {
+      this.profilePictureFileName = null;
+      this.profilePictureFile = null;
+      return;
+    }
+    const fileType = event.target.files[0].type;
+    if (!['image/jpeg', 'image/png'].includes(fileType)) {
+      // display validation error
+      this.validationLib.profilePicture.showErrMsg = true;
+      //reset properties from old file selection if present
+      this.profilePictureFileName = null;
+      this.profilePictureFile = null;
+      return
+    }
+    // hide  validation error, if on display
+    this.validationLib.profilePicture.showErrMsg = false;
+    this.profilePictureFileName = event.target.files[0]?.name;
+    this.profilePictureFile = event.target.files[0];
   }
 
   ngOnDestroy(): void {
-    
+    this.ngUnsub.next(true);
+    this.ngUnsub.complete();
   }
 }
