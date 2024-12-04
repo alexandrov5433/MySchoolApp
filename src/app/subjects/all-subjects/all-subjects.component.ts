152da12356193897d0c42636e8b4becc546b1b47
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-04 16:16:39 +0100

    Initial commit.

---
+++ src/app/subjects/all-subjects/all-subjects.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-subjects',
+  imports: [],
+  templateUrl: './all-subjects.component.html',
+  styleUrl: './all-subjects.component.css'
+})
+export class AllSubjectsComponent {
+
+}
