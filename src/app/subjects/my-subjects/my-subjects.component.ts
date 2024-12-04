ee18cb871241f8ce336f82c0121c687cf239d0b4
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-04 22:35:46 +0100

    View for personal subjects added.

--- src/app/subjects/my-subjects/my-subjects.component.ts
+++ src/app/subjects/my-subjects/my-subjects.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
+import { subjectCardsSearchResult } from '../../types/subjectCardsRearchResult';
+import { Router } from '@angular/router';
+import { SubjectsService } from '../../services/subjects.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import parseServerMsg from '../../util/parseServerMsg';
 
 @Component({
   selector: 'app-my-subjects',
@@ -7,5 +12,48 @@ import { Component } from '@angular/core';
   styleUrl: './my-subjects.component.css'
 })
 export class MySubjectsComponent {
+  subjectCards: WritableSignal<subjectCardsSearchResult | null> = signal(null);
+
+  constructor (
+    private router: Router,
+    private subjectsService: SubjectsService,
+    private snackBar: MatSnackBar
+  ) {}
+
+  showSnackBar(msg: string) {
+    this.snackBar.open(msg, 'OK', {
+      duration: 7000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom'
+    });
+  }
+
+  view(_id: string) {
+    console.log(_id);
+    //TODO redirect to subject
+  }
+
+  ngOnInit(): void {
+    //get all subjects
+    this.getMySubjects();
+  }
+
+  private getMySubjects() {
+    this.subjectsService.getSubjects('', '', true)
+      .subscribe({
+        next: val => {
+          const subjectCardsResult: subjectCardsSearchResult = parseServerMsg(val as string);
+          console.log(subjectCardsResult);
+          
+          this.subjectCards.set(subjectCardsResult);
+        },
+        error: err => {
+          const msg = parseServerMsg(err.error).msg;
+          console.error(msg);
+          this.showSnackBar(msg);
+        },
+        complete: () => {}
+      });
+  }
 
 }
