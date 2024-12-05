3c590f394ac6e10faa673c315578f7887e9cdcc0
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-05 22:32:21 +0100

    File download service in progress.

--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -7,17 +7,17 @@ import { environment as env } from '../../environments/environment.development';
   providedIn: 'root'
 })
 export class FileService {
-
+  blob:Blob | null = null;
   constructor(private http: HttpClient) { }
 
-/**
- * Returns one specific subject.
- * @param _id The _id of the Subject document in the DB.
- */
+  /**
+   * Returns one specific subject.
+   * @param _id The _id of the Subject document in the DB.
+   */
   getFileStreamById(_id: string) {
     return new Observable((subscriber) => {
       try {
-        this.http.get(`${env.restUrlBase}/file/${_id}`, {
+        this.http.get(`${env.restUrlBase}/file/stream/${_id}`, {
           responseType: 'blob',
           withCredentials: true,
           headers: {
@@ -25,6 +25,34 @@ export class FileService {
           }
         }).subscribe({
           next: val => subscriber.next(val),
+          error: err => subscriber.error(err),
+          complete: () => subscriber.complete()
+        });
+      } catch (e) {
+        subscriber.error(e);
+      }
+    });
+  }
+
+  getFileDownloadById(_id: string) {
+    return new Observable((subscriber) => {
+      try {
+        this.http.get(`${env.restUrlBase}/file/download/${_id}`, {
+          responseType: 'blob',
+          withCredentials: true,
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        }).subscribe({
+          next: (data: any) => {
+            this.blob = new Blob([data], {type: 'application/pdf'});
+
+            const downloadURL = window.URL.createObjectURL(data);
+            const link = document.createElement('a');
+            link.href = downloadURL;
+            link.download = "help.pdf";
+            link.click();
+          },
           error: err => subscriber.error(err),
           complete: () => subscriber.complete()
         });
