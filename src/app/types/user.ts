e10058ff8b3436ab909483a2b303484dcb1bb021
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-05 19:49:02 +0100

    Type for User added.

---
+++ src/app/types/user.ts
@@ -0,0 +1,23 @@
+export type User = {
+    _id: string,
+    status: string, //'parent', 'teacher', 'student'
+    firstName: string,
+    lastName: string,
+    dateOfBirth: string,
+    email: string,
+    mobileNumber: string,
+    homeNumber?: string,
+    street: string,
+    houseNumber: string,
+    city: string,
+    profilePicture: string,
+    //student
+    parentalAuthenticationCode?: string,
+    //student
+    uploadedDocuments?: Array<string>,
+    displayId: string,
+    //student
+    parents?: Array<string>,
+    //student
+    activeStudent?: boolean
+}
