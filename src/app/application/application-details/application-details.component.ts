4db8034c3e4bf7238e5aa965fc23982c78fd2e7a
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-05 22:29:55 +0100

    Application details view added.

--- src/app/application/application-details/application-details.component.ts
+++ src/app/application/application-details/application-details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, signal, Signal, WritableSignal } from '@angular/core';
+import { PendingApplicationService } from '../../services/pending-application.service';
+import { InterElementCommunicationService } from '../../services/inter-element-communication.service';
+import { Application } from '../../types/application';
 
 @Component({
   selector: 'app-application-details',
@@ -7,6 +10,21 @@ import { Component } from '@angular/core';
   styleUrl: './application-details.component.css',
   standalone: true
 })
-export class ApplicationDetailsComponent {
+export class ApplicationDetailsComponent implements OnInit {
+  private appId: string = '';
+  appData: WritableSignal<Application | null> = signal(null);
 
+  constructor (
+    private iec: InterElementCommunicationService
+  ) {}
+
+  @Input()
+  set _id(_id: string) {
+    this.appId = _id;
+  }
+
+  ngOnInit(): void {
+    this.appData.set(this.iec.pendingApplicationData());
+    console.log('app-details-cmp. _id: ', this.appData()?._id);
+  }
 }
