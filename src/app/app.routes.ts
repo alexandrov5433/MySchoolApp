c5a00d1a0c20712a1c31f762cccd4b2d0c3767f3
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-05 19:48:03 +0100

    Routes for application viewing added.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,29 +8,40 @@ import { AllSubjectsComponent } from './subjects/all-subjects/all-subjects.compo
 import { MySubjectsComponent } from './subjects/my-subjects/my-subjects.component';
 import { SubjectDetailsComponent } from './subjects/subject/subject-details/subject-details.component';
 import { PendingApplicationsComponent } from './application/pending-applications/pending-applications.component';
+import { ApplicationMainComponent } from './application/application-main/application-main.component';
+import { ApplicationDetailsComponent } from './application/application-details/application-details.component';
+import { ApplicationDocumentsComponent } from './application/application-documents/application-documents.component';
 
 export const routes: Routes = [
-    {path: '', redirectTo: '/home', pathMatch: 'full'},
-    {path: 'home', redirectTo: '/todo-page'},
-    {path: 'about', redirectTo: '/todo-page'},
-    {path: 'contact', redirectTo: '/todo-page'},
-    {path: 'faq', redirectTo: '/todo-page'},
-    {path: 'forms-and-documents', redirectTo: '/todo-page'},
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', redirectTo: '/todo-page' },
+    { path: 'about', redirectTo: '/todo-page' },
+    { path: 'contact', redirectTo: '/todo-page' },
+    { path: 'faq', redirectTo: '/todo-page' },
+    { path: 'forms-and-documents', redirectTo: '/todo-page' },
 
-    {path: 'log-in', component: LoginComponent},
-    {path: 'register', component: RegisterComponent},
-    {path: 'apply-now', component: ApplyNowComponent},
-    
-    {path: 'create-subject', component: CreateSubjectComponent},
-    {path: 'all-subjects', component: AllSubjectsComponent},
-    {path: 'my-subjects', component: MySubjectsComponent},
-    {path: 'subject-details/:_id', component: SubjectDetailsComponent, 
+    { path: 'log-in', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'apply-now', component: ApplyNowComponent },
+
+    { path: 'create-subject', component: CreateSubjectComponent },
+    { path: 'all-subjects', component: AllSubjectsComponent },
+    { path: 'my-subjects', component: MySubjectsComponent },
+    {
+        path: 'subject-details/:_id', component: SubjectDetailsComponent,
         children: [
-            
+
         ]
     },
 
-    {path: 'pending-applications', component: PendingApplicationsComponent},
+    { path: 'pending-applications', component: PendingApplicationsComponent },
+    {
+        path: 'application/:_id', component: ApplicationMainComponent, children: [
+            { path: 'details', component: ApplicationDetailsComponent },
+            { path: 'documents', component: ApplicationDocumentsComponent }
+        ]
+    },
 
-    {path: 'todo-page', component: TodoPageComponent},
+
+    { path: 'todo-page', component: TodoPageComponent },
 ];
