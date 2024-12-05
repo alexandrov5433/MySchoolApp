695d9b189647b5d791f41e2d842af588324c061f
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-05 12:22:17 +0100

    Main functionality added.

--- src/app/subjects/subject/subject-details/subject-details.component.ts
+++ src/app/subjects/subject/subject-details/subject-details.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, computed, Input, OnInit, Signal, signal, WritableSignal } from '@angular/core';
+import { SubjectDetails } from '../../../types/subjectDetails';
+import { SubjectsService } from '../../../services/subjects.service';
+import parseServerMsg from '../../../util/parseServerMsg';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { environment as env } from '../../../../environments/environment.development';
 
 @Component({
   selector: 'app-subject-details',
@@ -7,6 +12,52 @@ import { Component } from '@angular/core';
   styleUrl: './subject-details.component.css',
   standalone: true
 })
-export class SubjectDetailsComponent {
+export class SubjectDetailsComponent implements OnInit {
+  subjectDetails: WritableSignal<SubjectDetails | null> = signal(null);
+  subjectId: string = '';
+  teacherPicSrc: Signal<string> = computed(() => {
+    const picId: string = this.subjectDetails()?.subject.teacherPictureId || '';
+    return `${env.restUrlBase}/file/${picId}`;
+  });
+
+  constructor(
+    private subjectsService: SubjectsService,
+    private snackBar: MatSnackBar
+  ) { }
+
+  showSnackBar(msg: string) {
+    this.snackBar.open(msg, 'OK', {
+      duration: 7000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom'
+    });
+  }
+
+  @Input()
+  set _id(_id: string) {
+    this.subjectId = _id;
+  }
+
+  ngOnInit(): void {
+    console.log('_id recieved: ', this.subjectId);
+    this.subjectsService.getSubjectById(this.subjectId)
+      .subscribe({
+        next: val => {
+          const subjectResult: SubjectDetails = parseServerMsg(val as string);
+          console.log(subjectResult);
+          this.subjectDetails.set(subjectResult);
+        },
+        error: err => {
+          console.error(err);
+          const msg = parseServerMsg(err.error).msg;
+          this.showSnackBar(msg);
+        },
+        complete: () => { }
+      });
+  }
+
+  private getTeacherPicture(_id: string) {
+
+  }
 
 }
