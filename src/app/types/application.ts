6f652a3308b41c939fa1f88aeb28129f32b8ffa5
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-05 22:32:58 +0100

    Types modifications.

--- src/app/types/application.ts
+++ src/app/types/application.ts
@@ -1,8 +1,9 @@
+import { File } from "./file";
 import { User } from "./user";
 
 export type Application = {
     _id: string,
     status: string,
-    applicationDocuments: Array<string>,
+    applicationDocuments: Array<string> | Array<File>,
     applicant: User
 };
---
+++ src/app/types/file.ts
@@ -0,0 +1,8 @@
+export type File = {
+    _id: string,
+    originalName: string,
+    uniqueName: string,
+    pathToFile: string,
+    mimeType: string,
+    encoding: string,
+}
