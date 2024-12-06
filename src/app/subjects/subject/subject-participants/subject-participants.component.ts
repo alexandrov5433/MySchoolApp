f107414cd1af1e4837610df261a9fd3083bd7b49
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-06 20:50:19 +0100

    Participants view implemented.

--- src/app/subjects/subject/subject-participants/subject-participants.component.ts
+++ src/app/subjects/subject/subject-participants/subject-participants.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, signal, WritableSignal } from '@angular/core';
+import { SubjectsService } from '../../../services/subjects.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from '../../../services/user.service';
+import { Subject } from '../../../types/subject';
+import { environment as env } from '../../../../environments/environment.development';
+import { Subject as rxjsSubject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-subject-participants',
@@ -6,6 +12,66 @@ import { Component } from '@angular/core';
   templateUrl: './subject-participants.component.html',
   styleUrl: './subject-participants.component.css'
 })
-export class SubjectParticipantsComponent {
+export class SubjectParticipantsComponent implements OnInit, OnDestroy {
+  private ngDestroyer = new rxjsSubject();
+  subject: WritableSignal<Subject | null> = signal(null);
+  subjectId: string = '';
+  curUserStatus: WritableSignal<string> = signal('');
+  curUserId: WritableSignal<string> = signal('');
+  baseUrlForPicture: WritableSignal<string> = signal(`${env.restUrlBase}/file/stream/`);
 
+  constructor(
+    private subjectsService: SubjectsService,
+    private snackBar: MatSnackBar,
+    private userService: UserService
+  ) { }
+
+  showTeacherControls() {
+    return this.curUserStatus() === 'teacher' ? true : false;
+  }
+
+  @Input()
+  set _id(_id: string) {
+    this.subjectId = _id;
+  }
+
+  showSnackBar(msg: string) {
+    this.snackBar.open(msg, 'OK', {
+      duration: 7000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom'
+    });
+  }
+
+  private loadData() {
+    this.subjectsService.getSubjectById(this.subjectId)
+    .subscribe({
+      next: val => { },
+      error: err => {
+        console.error(err);
+        this.showSnackBar(err);
+      },
+      complete: () => {
+        this.subject.set(this.subjectsService.subjectData());
+        this.curUserStatus.set(this.userService.userAuthStatus);
+        this.curUserId.set(this.userService.user_Id);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.subjectsService.realoadDataTriggerForChildren
+    .pipe(takeUntil(this.ngDestroyer))
+    .subscribe({
+      next: val => {
+        this.loadData();
+      }
+    });
+    this.loadData();
+  }
+
+  ngOnDestroy(): void {
+    this.ngDestroyer.next(true);
+    this.ngDestroyer.complete();
+  }
 }
