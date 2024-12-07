e3131cc79708f691b397ff9dc1a55de57f3cd5d7
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-07 23:07:32 +0100

    File dolnloading redone and working.

--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -1,13 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
 import { environment as env } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FileService {
-  
+
   constructor(private http: HttpClient) { }
 
   /**
@@ -34,44 +34,38 @@ export class FileService {
     });
   }
 
-  getFileDownloadById(_id: string) {
-    // return new Observable((subscriber) => {
-    //   try {
-    //     this.http.get(`${env.restUrlBase}/file/download/${_id}`, {
-    //       responseType: 'blob',
-    //       withCredentials: true,
-    //       headers: {
-    //         'Content-Type': 'application/x-www-form-urlencoded'
-    //       }
-    //     }).subscribe({
-    //       next: (data: any) => {
-    //         this.blob = new Blob([data], {type: 'application/pdf'});
-
-    //         const downloadURL = window.URL.createObjectURL(data);
-    //         const link = document.createElement('a');
-    //         link.href = downloadURL;
-    //         link.download = "help.pdf";
-    //         link.click();
-    //       },
-    //       error: err => subscriber.error(err),
-    //       complete: () => subscriber.complete()
-    //     });
-    //   } catch (e) {
-    //     subscriber.error(e);
-    //   }
-    // });
-    try {
-      return this.http.get(`${env.restUrlBase}/file/download/${_id}`, {
-        responseType: 'blob',
-        withCredentials: true,
-        observe: 'body',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
-      });
-    } catch (e) {
-      return throwError(() => e);
-    }
-  }
+  // getFileDownloadById(_id: string) {
+  //   return new Observable((subscriber) => {
+  //     try {
+  //       this.http.get(`${env.restUrlBase}/file/download/${_id}`, {
+  //         responseType: 'blob' as 'json',
+  //         withCredentials: true,
+  //         // headers: {
+  //         //   'Content-Type': 'application/x-www-form-urlencoded'
+  //         // }
+  //       }).subscribe({
+  //         next: (data: any) => {
+  //           subscriber.next(data);
+  //         },
+  //         error: err => subscriber.error(err),
+  //         complete: () => subscriber.complete()
+  //       });
+  //     } catch (e) {
+  //       subscriber.error(e);
+  //     }
+  //   });
+  //   // try {
+  //   //   return this.http.get(`${env.restUrlBase}/file/download/${_id}`, {
+  //   //     responseType: 'blob',
+  //   //     withCredentials: true,
+  //   //     observe: 'body',
+  //   //     headers: {
+  //   //       'Content-Type': 'application/x-www-form-urlencoded'
+  //   //     }
+  //   //   });
+  //   // } catch (e) {
+  //   //   return throwError(() => e);
+  //   // }
+  // }
 
 }
