a96054d7506852a95412a1e3b25a5f017f8c2514
alexandrov5433 <alexandrov5433@gmail.com>
2024-12-10 17:11:30 +0100

    Home menu buttons change based on user status.

--- src/app/basic/home/home.component.ts
+++ src/app/basic/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, Signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,13 @@ import { RouterLink } from '@angular/router';
   standalone: true
 })
 export class HomeComponent {
-
+  isUserLoggedIn: Signal<boolean> = computed(() => {
+    return this.userService.isUserLoggedIn;
+  });
+  userId: Signal<string> = computed(() => {
+    return this.userService.user_Id;
+  });
+  constructor(
+    private userService: UserService
+  ) {}
 }
